49cb13763be468815163b04e44adce4cb108eef3
kevincrafts <kevin.crafts@colorado.edu>
2016-06-17 16:36:47 -0600

    massive update

--- modules/custom/cu_newsletter/templates/node--newsletter.tpl.php
+++ modules/custom/cu_newsletter/templates/node--newsletter.tpl.php
@@ -1,36 +1,37 @@
-<p class="date"><?php print $ap_date_cu_medium_date; ?></p>
+<div class="newsletter-wrapper row clearfix">
+  <div class="newsletter-main <?php print $column_classes['main']; ?>">
+    <?php
+      if (!empty($content['field_newsletter_intro_image'])): ?>
+      <div class="newsletter-hero">
+        <?php
+        $content['field_newsletter_intro_image'][0]['#image_style'] = 'email_large';
+        print render($content['field_newsletter_intro_image']);
+        ?>
+      </div>
 
-<?php
-  if (!empty($content['field_newsletter_intro_image'])) {
-    $content['field_newsletter_intro_image'][0]['#image_style'] = 'email_feature_thumbnail_large';
-    print render($content['field_newsletter_intro_image']);
-  }
-?>
-
-<?php if (!empty($content['body'])): ?>
-  <div class="newsletter-intro">
-    <?php print render($content['body']); ?>
+    <?php endif; ?>
+    <?php if (!empty($content['body'])): ?>
+      <div class="newsletter-intro">
+        <?php print render($content['body']); ?>
+      </div>
+    <?php endif; ?>
+    <?php print render($content['field_newsletter_section']); ?>
   </div>
-<?php endif; ?>
-<?php print render($content['field_newsletter_section']); ?>
-<div class="newsletter-ad-promo-wrapper">
-  <?php print render($content['field_newsletter_ad_promo'][0]); ?>
-  <?php print render($content['field_newsletter_ad_promo'][1]); ?>
-</div>
-  <?php
-    $blocks = $content['field_newsletter_text_block'];
-    $columns = 2;
-    $chunks = array_chunk($blocks['#items'], $columns, true);
+  <?php if (!empty($column_classes['sidebar'])): ?>
+    <div class="newsletter-sidebar <?php print $column_classes['sidebar']; ?>">
+      <div class="newsletter-ad-promo-wrapper">
+        <?php if (!empty($content['field_newsletter_ad_promo'][0])): ?>
+          <?php print render($content['field_newsletter_ad_promo'][0]); ?>
+        <?php endif; ?>
+        <?php if (!empty($content['field_newsletter_text_block'])): ?>
+          <?php print render($content['field_newsletter_text_block']); ?>
+        <?php endif; ?>
+        <?php if (!empty($content['field_newsletter_ad_promo'][1])): ?>
+          <?php print render($content['field_newsletter_ad_promo'][1]); ?>
+        <?php endif; ?>
+      </div>
 
-    foreach ($chunks as $chunk) {
-      print '<div class="newsletter-text-block-wrapper clearfix">';
-        foreach ($chunk as $key => $block) {
-          print render($content['field_newsletter_text_block'][$key]);
-        }
-        for ($i = count($chunk); $i < $columns; $i++) {
-          print '<div class="newsletter-text-block"></div>';
-        }
-      print '</div>';
-    }
-   ?>
-  <?php  ?>
+      <?php  ?>
+    </div>
+  <?php endif; ?>
+</div>
