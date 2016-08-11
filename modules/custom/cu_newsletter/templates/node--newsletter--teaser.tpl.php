5b8b47ebd5e78847b03de178e2a696d37dbdfec8
kevincrafts <kevin.crafts@colorado.edu>
2016-08-11 12:47:21 -0600

    adding default heading tag

--- modules/custom/cu_newsletter/templates/node--newsletter--teaser.tpl.php
+++ modules/custom/cu_newsletter/templates/node--newsletter--teaser.tpl.php
@@ -1,6 +1,6 @@
 <?php
   $title_class = (empty($content['body']) && empty($content['field_newsletter_intro_image'])) ? 'node-title-only' : '';
-  //$heading_tag['#tag'] = 'strong';
+  $heading_tag['#tag'] = !empty($heading_tag['#tag']) ? $heading_tag['#tag'] : 'h2';
 ?>
 <div class="newsletter-view-mode-teaser node-view-mode-teaser clearfix">
   <<?php print $heading_tag['#tag']; ?> class="<?php print $title_class; ?>"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></<?php print $heading_tag['#tag']; ?>>
